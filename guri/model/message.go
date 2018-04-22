875079af375fff8fa2f1e14c32a2d8eed9b19cc8
guri3 <aloesquash.lock@gmail.com>
2018-04-22 13:35:38 +0900

    Update MessageByID

--- guri/model/message.go
+++ guri/model/message.go
@@ -42,7 +42,7 @@ func MessageByID(db *sql.DB, id string) (*Message, error) {
 	m := &Message{}
 
 	// 1-1. ユーザー名を表示しよう
-	if err := db.QueryRow(`select id, body from message where id = ?`, id).Scan(&m.ID, &m.Body); err != nil {
+	if err := db.QueryRow(`select id, body username from message where id = ?`, id).Scan(&m.ID, &m.Body); err != nil {
 		return nil, err
 	}
 
