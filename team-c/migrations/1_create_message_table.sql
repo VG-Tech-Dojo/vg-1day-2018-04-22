ce60b8a12ee14a436b2621f5fdc96e833eb27c7f
tockn <takuto.sato.5g@stu.hosei.ac.jp>
2018-04-22 08:11:01 +0000

    Change migration #151

--- team-c/migrations/1_create_message_table.sql
+++ team-c/migrations/1_create_message_table.sql
@@ -3,9 +3,14 @@ CREATE TABLE message (
     id INTEGER NOT NULL PRIMARY KEY,
     body TEXT NOT NULL DEFAULT "",
     username TEXT NOT NULL DEFAULT "",
+    image_id INTEGER NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT (DATETIME('now', 'localtime')),
     updated TIMESTAMP NOT NULL DEFAULT (DATETIME('now', 'localtime'))
 );
-
+CREATE TABLE image (
+    id INTEGER NOT NULL PRIMARY KEY,
+    path TEXT NOT NULL
+);
 -- +migrate Down
 DROP TABLE message;
+
